b300d6bda3e17a7bb737d55dbcbda4868bb87f64
Aung Chan Myae <aungchanmyae10997@gmail.com>
2024-12-05 15:58:15 +0700

    fixing the error sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,39 +1,41 @@
-import "./Sidebar.css"
-import { Link } from "react-scroll"
-import { navRoutes } from "../../data"
-import { FaTimes } from "react-icons/fa"
-import { cssPerfectShape } from "../../ulti"
+import "./Sidebar.css";
+import { Link } from "react-scroll";
+import { navRoutes } from "../../data";
+import { FaTimes } from "react-icons/fa";
+import { cssPerfectShape } from "../../ulti";
 
-const Sidebar = ({open, onClose}) => {
+const Sidebar = ({ open, onClose }) => {
+  console.log("Sidebar visibility:", open);
   return (
     <div className={`sidebar ${open && "active"}`}>
       <div>
         <div className="top">
-          <div className="close-btn"
-          onClick={onClose}
-          style={{...cssPerfectShape(40,40)}}>
+          <div
+            className="close-btn"
+            onClick={() => onClose(false)}
+            style={{ ...cssPerfectShape(40, 40) }}
+          >
             <FaTimes />
           </div>
         </div>
         <div className="middle">
-        {
-            navRoutes.map(((route,index)=>(
-              <Link 
-                to={route.id} 
-                className="route" 
-                activeClass="active" 
-                smooth={true} spy={true} 
-                offset={-50} 
-                key={index}
-              > 
-                {route.name}
-              </Link>
-            )))
-            }
+          {navRoutes.map((route, index) => (
+            <Link
+              to={route.id}
+              className="route"
+              activeClass="active"
+              smooth={true}
+              spy={true}
+              offset={-50}
+              key={index}
+            >
+              {route.name}
+            </Link>
+          ))}
         </div>
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/segments/Navbar/index.jsx
+++ src/segments/Navbar/index.jsx
@@ -1,6 +1,6 @@
-import DataWaveLogo from "../../components/DataWaveLogo"
-import "./Navbar.css"
-import {navRoutes} from "../../data";
+import DataWaveLogo from "../../components/DataWaveLogo";
+import "./Navbar.css";
+import { navRoutes } from "../../data";
 import { Link } from "react-scroll";
 import Social from "../../components/Social";
 import { CgMenuRight } from "react-icons/cg";
@@ -9,38 +9,46 @@ import { useState } from "react";
 // social is missing 's' key
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
-  const [open,setOpen] = useState(false)
+  // console.log("Sidebar open state:", open);
 
   return (
     <>
-      <Sidebar open={open} onClose={()=>setOpen(!open)} />
+      <Sidebar open={open} onClose={() => setOpen(!open)} />
+      {open && (
+        <div className="sidebar-overlay" onClick={() => setOpen(!open)}></div>
+      )}
       <nav id="navbar">
         <DataWaveLogo />
         <div className="route-wrapper">
-          {
-          navRoutes.map(((route,index)=>(
-            <Link 
-              to={route.id} 
-              className="route" 
-              activeClass="active" 
-              smooth={true} spy={true} 
-              offset={-50} 
+          {navRoutes.map((route, index) => (
+            <Link
+              to={route.id}
+              className="route"
+              activeClass="active"
+              smooth={true}
+              spy={true}
+              offset={-50}
               key={index}
-            > 
+            >
               {route.name}
             </Link>
-          )))
-          }
+          ))}
         </div>
         <Social />
-        <div className="menu" onClick={()=>setOpen(!open)}>
+        <div
+          className="menu"
+          onClick={() => {
+            setOpen(!open);
+            // console.log("Menu button clicked, open state:", !open);
+          }}
+        >
           <CgMenuRight />
         </div>
       </nav>
     </>
-    
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
